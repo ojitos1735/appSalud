6076971ebb51aa7835fdaf5abfdd23332fa46b40
Analia Quevedo <anyg_1735@hotmail.com>
2023-02-16 21:37:32 -0300

    actualización desde UML

---
+++ src/main/java/com/salud/sistema/entidades/HistoriaClinica.java
@@ -0,0 +1,12 @@
+package com.salud.sistema.entidades;
+
+public class HistoriaClinica {
+
+    private Long id;
+
+    private Paciente paciente;
+
+    private List<DetalleHC> detalles;
+
+    private List<Profesional> profesionales;
+}
--- src/main/java/com/salud/sistema/enums/EspecialidadesEnun.java
+++
@@ -1,7 +0,0 @@
-
-package com.salud.sistema.enums;
-
-
-public class EspecialidadesEnun {
-    
-}
