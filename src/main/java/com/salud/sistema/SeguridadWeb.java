9d880a9dac25ac0c10b8f7b77201407bdbf5b35e
lucas <84870866+Ldev777@users.noreply.github.com>
2023-03-14 19:12:35 -0300

    agregado de boton logout en index

--- src/main/java/com/salud/sistema/SeguridadWeb.java
+++ src/main/java/com/salud/sistema/SeguridadWeb.java
@@ -36,7 +36,7 @@ public class SeguridadWeb extends WebSecurityConfigurerAdapter {
                         .loginProcessingUrl("/logincheck")
                         .usernameParameter("email")
                         .passwordParameter("password")
-                        .defaultSuccessUrl("/inicio")
+                        .defaultSuccessUrl("/")
                         .permitAll()
                 .and().logout()
                         .logoutUrl("/logout")
